76c86ce8309d701a5308193b4908516bcc2b8152
ModeDerp <modederp@hotmail.com>
2017-11-08 15:04:53 +0100

    Lade in koden för den första metoden till den 4:e metoden temporärt

--- src/grafik/main.java
+++ src/grafik/main.java
@@ -44,7 +44,7 @@ public class main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 
 		// Ändra siffran här nere till 1,2,3 eller 4 beroende på vilken vy du vill se!
-		makeView1();
+		makeView4();
 
 		primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
 		primaryStage.show();
@@ -135,6 +135,36 @@ public class main extends Application {
 		 * TODO: Typ samma som makeView1 fast med Circle istället för Rectangle,
 		 * eller hur??
 		 */
+		
+		int rows = 10;
+		int cols = 10;
+
+		double width = WIDTH / cols;
+		double height = HEIGHT / rows;
+
+		for (int row = 0; row < rows; row++) {
+
+			for (int col = 0; col < cols; col++) {
+
+				// Skapa en ny Rectangle
+				Rectangle rect = new Rectangle(width, height);
+
+				// Flytta rect i x-led så den kommer rätt
+				rect.setTranslateX(col * width);
+
+				// Flytta rect i y-led så den kommer rätt
+				rect.setTranslateY(row * height);
+
+				// Färglägg med en random färg
+				int r = (int) (Math.random() * 255);
+				int g = (int) (Math.random() * 255);
+				int b = (int) (Math.random() * 255);
+				rect.setFill(Color.rgb(r, g, b));
+
+				// Lägg till rect så den syns i fönstret
+				root.getChildren().add(rect);
+			}
+		}
 
 	}
 
